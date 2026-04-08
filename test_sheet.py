b5c65940311177811fe731fe8caf533959fb4722
Longevite <longevite.natural@gmail.com>
2026-04-08 11:48:49 -0500

    cambios

--- test_sheet.py
+++ test_sheet.py
@@ -1,7 +1,7 @@
 import asyncio
 import httpx
 
-SHEET_ID = "1N3xGYFlSsKrUFV6JtrkeBQ_Acc-9ypXlNc9H74qT7N8"
+SHEET_ID = "1Cg1SBYIq2kiPYI-D8mKKlvt1dLC8Zz045jIWpK3Ho0k"
 
 async def test():
     url = "https://docs.google.com/spreadsheets/d/" + SHEET_ID + "/gviz/tq?tqx=out:csv&sheet=Catalogo"
